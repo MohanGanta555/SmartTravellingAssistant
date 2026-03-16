238dbf2c64eb09f5d2a59fa5878b6fd9104cce6b
MohanGanta555 <mohanganta555@gmail.com>
2026-03-16 23:24:51 +0530

    Updated CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
